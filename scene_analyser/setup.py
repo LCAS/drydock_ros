48c7f96a7ab941b5ee1fe2a8c8ac6f85c2ec92b3
FastPick User (marc@hanheide.net) <marc@hanheide.net>
2022-03-09 11:47:09 +0000

    include MaskPredictor in setup.py

--- scene_analyser/setup.py
+++ scene_analyser/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=[ 'scene_analyser' ],
+    packages=[ 'scene_analyser', 'MaskPredictor' ],
     package_dir={'': 'src'}
 )
 
